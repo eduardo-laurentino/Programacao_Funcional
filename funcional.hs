392d74302f746bd35a4a5d608273f73cf5a3b273
Eduardo Laurentino <97697621+eduardo-laurentino@users.noreply.github.com>
2023-05-09 18:10:21 -0300

    commit

--- funcional.hs
+++ funcional.hs
@@ -119,6 +119,13 @@ somaDigitos x
     |x < 10 = x
     |otherwise = (mod x 10) + somaDigitos(div x 10)
 
+--Função que multiplica dois números
+multiplicacao :: Int -> Int -> Int
+multiplicacao x y 
+    |x == 0 || y == 0 = 0
+    |y > 0 = x + multiplicacao x (y-1)
+    |otherwise = negate (multiplicacao x (negate y))
+
 --Função que retorna o0 resultado de uma potência
 potencia :: Int -> Int ->  Int
 potencia x y
