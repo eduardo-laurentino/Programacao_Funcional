2973f714fca97e0cd9980da9e861bde5ad613855
Eduardo Laurentino <97697621+eduardo-laurentino@users.noreply.github.com>
2023-07-10 21:41:00 -0300

    commit

--- n3.hs
+++ n3.hs
@@ -2,8 +2,9 @@ import System.IO
 import Data.Char 
 import Data.String
 
+
 -- Definindo o tipo de dados para o registro
-data Pessoa = Pessoa
+data Registro = Pessoa
   { nome :: String,
   idade :: Int,
   rua :: String,
@@ -11,113 +12,41 @@ data Pessoa = Pessoa
   cidade :: String
   } deriving (Eq, Show, Ord)
 
--- Definindo a base de dados como uma lista de pessoas
-baseDados :: [Pessoa]
-baseDados = [Pessoa "Joao" 25 "Rua A" 20 "Caxias",
-  Pessoa "Maria" 30 "Rua B" 21 "Timon",
-  Pessoa "Carlos" 40 "Rua C" 21 "Caxias",
-  Pessoa "Joao" 25 "Rua A" 20 "Caxias",
-  Pessoa "Maria" 30 "Rua B" 21 "Timon",
-  Pessoa "Carlos" 40 "Rua C" 21 "Caxias"]
-
--- Função que mostra o menu e retorna a opção escolhida
-mostrarMenu :: IO ()
-mostrarMenu = do
-  putStrLn "===== MENU ====="
-  putStrLn "1. BUSCA PESSOA"
-  putStrLn "2. CADASTRAR PESSOA"
-  putStrLn "3. ATUALIZAR DADOS PESSOA"
-  putStrLn "4. RELATÓRIO"
-  putStrLn "5. IDADE MÉDIA POPULAÇÃO"
-  putStrLn "6. VISUALIZAR DADOS"
-  putStrLn "7. SAIR"
-  putStrLn "================="
-  putStrLn "Escolha uma opção: "
-  opcao <- getChar
-  putStrLn ""
-  executarOpcao opcao
-  mostrarMenu
-
--- Função que executa a opção escolhida pelo usuário
-executarOpcao :: Char -> IO ()
-executarOpcao '1' = dadosBuscaPessoa
-executarOpcao '2' = cadastrarPessoa
-executarOpcao '3' = putStrLn "Você escolheu a opção 3."
-executarOpcao '4' = dadosBuscaCidade
-executarOpcao '5' = calculaMediaIdade
-executarOpcao '6' = print baseDadosOrdenada
-executarOpcao '7' = putStrLn "Saindo..."
-executarOpcao _ = putStrLn "Opção inválida."
-
--- Função para buscar uma pessoa pelo nome na base de dados
-buscarPessoa :: String -> [Pessoa] -> Maybe Pessoa
-buscarPessoa _ [] = Nothing
-buscarPessoa name (pessoa : pessoas)
-  | name == (nome pessoa) = Just pessoa
-  | otherwise = buscarPessoa name pessoas
-
-dadosBuscaPessoa :: IO ()
-dadosBuscaPessoa = do
-  putStrLn "Digite o nome da pessoa que você deseja buscar:"
-  nome <- getLine
-  let pessoaEncontrada = buscarPessoa nome baseDados
-  case pessoaEncontrada of
-    Just pessoa -> putStrLn $ "Pessoa encontrada: " ++ show pessoa
-    Nothing -> putStrLn "Pessoa não encontrada."
-
-
--- Função para calcular a média de idade das pessoas cadastradas na base de dados
-auxCalcularMediaIdade :: [Pessoa] -> Float
-auxCalcularMediaIdade registros =
-  let totalIdades = fromIntegral $ sum $ map idade baseDados
-      quantidadeRegistros = fromIntegral $ length baseDados
-  in totalIdades / quantidadeRegistros
-
-calculaMediaIdade :: IO ()
-calculaMediaIdade = do
-  let mediaIdade = auxCalcularMediaIdade baseDados
-  putStrLn $ "A média de idade é: " ++ show mediaIdade
-
+imprimir :: [Registro] -> IO ()
+imprimir [] = do putStrLn "\nBASE DE DADOS VAZIA"
+imprimir lista = do
+  putStrLn "\n--------BASE DE DADOS--------"
+  print lista
 
 -- Função para buscar na base de dados a quantidade de pessoas morando em uma cidade
-buscarCidade :: String -> [Pessoa] -> Int
+buscarCidade :: String -> [Registro] -> Int
 buscarCidade _ [] = 0
 buscarCidade name (pessoa : pessoas)
   | name == (cidade pessoa) = 1+ buscarCidade name pessoas
   | otherwise = buscarCidade name pessoas
 
-dadosBuscaCidade :: IO ()
-dadosBuscaCidade = do
+dadosBuscaCidade ::[Registro] -> IO ()
+dadosBuscaCidade baseDados = do
   putStrLn "Digite a cidade que deseja gerar relatório:"
   nome <- getLine
   let cidadeEncontrada = buscarCidade nome baseDados
   case cidadeEncontrada of
     cidade -> putStrLn $ "Pessoas morando nesta cidade: " ++ show cidade
 
--- Função para cadastrar uma pessoa na base de dados
-adicionarPessoa :: Pessoa -> [Pessoa] -> [Pessoa]
-adicionarPessoa pessoa base = [pessoa] ++ base
+-- Função para calcular a média de idade das pessoas cadastradas na base de dados
+auxCalcularMediaIdade :: [Registro] -> Float
+auxCalcularMediaIdade registros =
+  let totalIdades = fromIntegral $ sum $ map idade registros
+      quantidadeRegistros = fromIntegral $ length registros
+  in totalIdades / quantidadeRegistros
 
-cadastrarPessoa :: IO ()
-cadastrarPessoa = do
-  putStrLn "Digite o nome da Pessoa:"
-  nome <- getLine
-  putStrLn "Digite a Idade:"
-  idade <- getLine
-  putStrLn "Digite o nome da rua:"
-  rua <- getLine
-  putStrLn "Digite o número da casa:"
-  casa <- getLine
-  putStrLn "Digite o nome da cidade:"
-  cidade <- getLine
-  let novaPessoa = Pessoa nome (read idade :: Int) rua (read casa :: Int) cidade
-  let novaBaseDados = adicionarPessoa novaPessoa baseDados
-  putStrLn "Pessoa cadastrada com Sucesso !!"
-  print novaBaseDados
+calculaMediaIdade :: [Registro] -> IO ()
+calculaMediaIdade baseDados = do
+  let mediaIdade = auxCalcularMediaIdade baseDados
+  putStrLn $ "A média de idade é: " ++ show mediaIdade
 
-
--- Função que ordena uma lista de inteiros
-compararPorNome :: Pessoa -> Pessoa -> Ordering
+-- Função que ordena a base de dados
+compararPorNome :: Registro -> Registro -> Ordering
 compararPorNome p1 p2 = compare (nome p1) (nome p2)
 
 quicksort :: (a -> a -> Ordering) -> [a] -> [a]
@@ -127,5 +56,71 @@ quicksort cmp (x:xs) =
       maiores = quicksort cmp [a | a <- xs, cmp a x /= LT]
   in menores ++ [x] ++ maiores
 
-baseDadosOrdenada :: [Pessoa]
-baseDadosOrdenada = quicksort compararPorNome baseDados
+baseDadosOrdenada :: [Registro] -> [Registro]
+baseDadosOrdenada pessoa = quicksort compararPorNome pessoa
+
+-- Função para buscar na base de dados a quantidade de pessoas morando em uma cidade
+buscarPessoa :: String -> [Registro] -> IO ()
+buscarPessoa _ [] = putStrLn "Base de dados vazia"
+buscarPessoa name (pessoa : pessoas)
+  | name == (nome pessoa) = putStrLn "Pessoa encontrada"
+  | pessoas == [] = putStrLn "Pessoa não encontrada"
+  | otherwise = buscarPessoa name pessoas
+
+-- Função para adicionar uma pessoa na base de dados
+adicionarPessoa :: [Registro] -> IO [Registro]
+adicionarPessoa dados = do
+  putStrLn "\n-------ADICIONAR-------"
+  putStrLn "Digite o nome"
+  nome <- getLine
+  putStrLn "Digite a idade"
+  idade <- getLine
+  putStrLn "Digite o nome da rua"
+  rua <- getLine
+  putStrLn "Digite o número da casa"
+  casa <- getLine
+  putStrLn "Digite a cidade"
+  cidade <- getLine
+  let novaPessoa = Pessoa nome (read idade :: Int) rua (read casa :: Int) cidade
+  return (novaPessoa:dados)
+
+menu :: [Registro] -> IO ()
+menu dados = do
+  putStrLn "\n--------MENU--------"
+  putStrLn "Digite 1 para inserir uma pessoa"
+  putStrLn "Digite 2 imprimir"
+  putStrLn "Digite 3 para gerar relatório"
+  putStrLn "Digite 4 para calcular a média de iddade"
+  putStrLn "Digite 5 para atualizar um registro da base de dados"
+  putStrLn "Digite 0 para sair"
+  putStr "Opção: "
+  opt <- getChar
+  getChar
+  case opt of
+    '1' -> do
+      db <- adicionarPessoa dados
+      putStrLn "Pessoa Adicionada"
+      menu db
+    '2' -> do
+      imprimir (baseDadosOrdenada dados)
+      menu dados
+    '3' -> do
+      dadosBuscaCidade dados
+      menu dados
+    '4' -> do
+      calculaMediaIdade dados
+      menu dados
+    '5' -> do
+      putStrLn "Digite o nome da pessoa:"
+      nome <- getLine
+      buscarPessoa nome dados
+    '0' -> do
+      putStrLn "\n--------FIM--------"
+    _ -> do
+      putStrLn "\nOpção inválida!"
+      menu dados
+
+main :: IO ()
+main = do
+  menu []
+  return ()
